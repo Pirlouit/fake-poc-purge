381c8c21a8e0ee2edda6015dfd583db2eb8be104
Pirlouit PROVIS <pirlouit.provis@axa.fr>
2022-06-17 11:28:21 +0200

    fix build issues

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -8,18 +8,18 @@
 // You can also remove this file if you'd prefer not to use a
 // service worker, and the Workbox build step will be skipped.
 
-import { clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
-import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
-import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
-import { logToDashboard } from "./utils";
+import { clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+import { logToDashboard } from './utils';
 
 declare const self: ServiceWorkerGlobalScope;
 
-const CACHE_NAME = "rendezvous-data-v1";
+// const CACHE_NAME = 'rendezvous-data-v1';
 
-const DONT_DELETE_RESOURCES = ["/assets/img/"];
+// const DONT_DELETE_RESOURCES = ['/assets/img/'];
 
 clientsClaim();
 
@@ -32,85 +32,85 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
+const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }: { request: Request; url: URL }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== "navigate") {
-      return false;
-    }
+    // Return false to exempt requests from being fulfilled by index.html.
+    ({ request, url }: { request: Request; url: URL }) => {
+        // If this isn't a navigation, skip.
+        if (request.mode !== 'navigate') {
+            return false;
+        }
 
-    // If this is a URL that starts with /_, skip.
-    if (url.pathname.startsWith("/_")) {
-      return false;
-    }
+        // If this is a URL that starts with /_, skip.
+        if (url.pathname.startsWith('/_')) {
+            return false;
+        }
 
-    // If this looks like a URL for a resource, because it contains
-    // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+        // If this looks like a URL for a resource, because it contains
+        // a file extension, skip.
+        if (url.pathname.match(fileExtensionRegexp)) {
+            return false;
+        }
 
-    // Return true to signal that we want to use the handler.
-    return true;
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+        // Return true to signal that we want to use the handler.
+        return true;
+    },
+    createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'),
 );
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith(".png"),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
-    ],
-  })
+    // Add in any other file extensions or routing criteria as needed.
+    ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+    // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    new StaleWhileRevalidate({
+        cacheName: 'images',
+        plugins: [
+            // Ensure that once this runtime cache reaches a maximum size the
+            // least-recently used images are removed.
+            new ExpirationPlugin({ maxEntries: 50 }),
+        ],
+    }),
 );
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
 // Any other custom service worker logic can go here.
-self.addEventListener("periodicsync", (event: any) => {
-  logToDashboard("addListener for periodicsync");
-  if (event.tag === "delete-cached-content") {
-    logToDashboard(new Date().toLocaleTimeString());
-    // event.waitUntil(deleteCachedContent());
-  }
+self.addEventListener('periodicsync', (event: any) => {
+    logToDashboard('addListener for periodicsync');
+    if (event.tag === 'delete-cached-content') {
+        logToDashboard(new Date().toLocaleTimeString());
+        // event.waitUntil(deleteCachedContent());
+    }
 });
 
-async function deleteCachedContent() {
-  const requests = await findCacheEntriesToDeleted();
-  const cache = await caches.open(CACHE_NAME);
+// async function deleteCachedContent() {
+//   const requests = await findCacheEntriesToDeleted();
+//   const cache = await caches.open(CACHE_NAME);
 
-  for (const request of requests) {
-    try {
-      // Fetch the new version.
-      const fetchResponse = await fetch(request);
-      // Refresh the cache.
-      await cache.put(request, fetchResponse.clone());
-    } catch (e) {
-      // Fail silently, we'll just keep whatever we already had in the cache.
-    }
-  }
-}
+//   for (const request of requests) {
+//     try {
+//       // Fetch the new version.
+//       const fetchResponse = await fetch(request);
+//       // Refresh the cache.
+//       await cache.put(request, fetchResponse.clone());
+//     } catch (e) {
+//       // Fail silently, we'll just keep whatever we already had in the cache.
+//     }
+//   }
+// }
 
-async function findCacheEntriesToDeleted() {
-  const cache = await caches.open(CACHE_NAME);
-  const requests = await cache.keys();
-  return requests.filter((request) => {
-    return !DONT_DELETE_RESOURCES.some((pattern) => request.url.includes(pattern));
-  });
-}
+// async function findCacheEntriesToDeleted() {
+//   const cache = await caches.open(CACHE_NAME);
+//   const requests = await cache.keys();
+//   return requests.filter((request) => {
+//     return !DONT_DELETE_RESOURCES.some((pattern) => request.url.includes(pattern));
+//   });
+// }
